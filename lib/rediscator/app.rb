db6626606de6db38188bfd8741bf0fa95846d46e
Sam Stokes <me@samstokes.co.uk>
2011-06-03 17:37:30 -0700

    Set up a cron entry to daily backup the Redis dump file

--- lib/rediscator/app.rb
+++ lib/rediscator/app.rb
@@ -9,7 +9,7 @@ module Rediscator
     include Thor::Actions
     include Util
 
-    REQUIRED_PACKAGES = %w(git-core build-essential tcl8.5 pwgen)
+    REQUIRED_PACKAGES = %w(git-core build-essential tcl8.5 pwgen s3cmd)
     REDIS_USER = 'redis'
 
     CONFIG_SUBSTITUTIONS = {
@@ -24,9 +24,17 @@ module Rediscator
     desc 'setup', 'Set up Redis'
     method_option :redis_version, :required => true, :desc => "Version of Redis to install"
     method_option :run_tests, :default => false, :type => :boolean, :desc => "Whether to run the Redis test suite"
+    method_option :backup_tempdir, :default => '/tmp', :desc => "Temporary directory for daily backups"
+    method_option :backup_s3_prefix, :required => true, :desc => "S3 bucket and prefix for daily backups, e.g. s3://backups/redis"
     def setup
       redis_version = options[:redis_version]
       run_tests = options[:run_tests]
+      backup_tempdir = options[:backup_tempdir]
+      backup_s3_prefix = options[:backup_s3_prefix]
+
+      redis_dir = "redis-#{redis_version}"
+      redis_path = "~#{REDIS_USER}/opt/#{redis_dir}"
+      rediscator_path = File.join(Dir.pwd, File.dirname(__FILE__), '..', '..')
 
       package_install! *REQUIRED_PACKAGES
 
@@ -49,7 +57,6 @@ module Rediscator
               run! :make, :test if run_tests
             end
 
-            redis_dir = "redis-#{redis_version}"
             run! :mkdir, '-p', *%w(bin etc log tmp).map {|dir| "#{redis_dir}/#{dir}" }
             inside redis_dir do
               pwd = Dir.pwd
@@ -101,6 +108,38 @@ module Rediscator
                 puts "\t#{property}:\t#{value}"
               end
             end
+          end
+
+          run! *%w(mkdir -p bin)
+          run! *%W(cp #{rediscator_path}/bin/s3_gzbackup bin)
+
+          sudo! :mkdir, '-p', backup_tempdir
+          sudo! :chmod, 'a+rwxt', backup_tempdir
+
+          backup_command = %W(
+            ~#{REDIS_USER}/bin/s3_gzbackup
+            --temp-dir='#{backup_tempdir}'
+            #{redis_path}/dump.rdb
+            '#{backup_s3_prefix}'
+          ).join(' ')
+
+          crontab_entry = <<-CRONTAB
+# m h  dom mon dow command
+42  03 *   *   *   #{backup_command}
+          CRONTAB
+
+          old_crontab = begin
+                          run! :crontab, '-l'
+                        rescue ShellError => se
+                          if se.message =~ /\bno crontab for #{REDIS_USER}\b/
+                            ''
+                          else
+                            raise
+                          end
+                        end
+          if old_crontab.grep(/#{Regexp.escape backup_command}$/).empty?
+            new_crontab = old_crontab + "\n" + crontab_entry
+            run! :crontab, '-', :stdin => new_crontab
           end
         end
       end
