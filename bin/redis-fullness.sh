b15ad413a1f1a453ea5b3dc282181f1f64b24286
Sam Stokes <me@samstokes.co.uk>
2011-06-20 16:43:47 -0700

    Full path in redis-fullness.sh script

--- bin/redis-fullness.sh
+++ bin/redis-fullness.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
-used=$(redis-metric.sh used_memory)
+used=$($HOME/bin/redis-metric.sh used_memory)
 sudo -u redis /home/redis/bin/authed-redis-cli CONFIG GET maxmemory \
   | awk -vused="$used" '/^[0-9]+$/ { print 100 * used / $1 }'
